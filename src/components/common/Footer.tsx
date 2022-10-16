8328b57ca7bf866dece48c28a9bbc0904a990286
juancgalueweb <juancgalue@icloud.com>
2022-10-16 02:44:01 -0300

    Add a carousel with 5 images in homepage

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <footer className="flex p-6 h-20 text-black bg-slate-100 items-center justify-center">
+    <footer className="flex p-6 h-10 text-black bg-white items-center justify-center">
       <a
         href="https://rootstack.com/en"
         target="_blank"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,68 @@
 import { NextPage } from "next";
+import Image from "next/image";
+import slice1 from "../../public/images/bg-cocktail1.jpg";
+import slice2 from "../../public/images/bg-cocktail2.jpg";
+import slice3 from "../../public/images/bg-cocktail3.jpg";
+import slice4 from "../../public/images/bg-cocktail4.jpg";
+import slice5 from "../../public/images/bg-cocktail5.jpg";
+import { Carousel } from "antd";
 import { ApplicationWrapper } from "../components/layout/ApplicationWrapper";
 
 const Home: NextPage = () => {
   const titleMessage = "Home";
-  const descriptionMessage = "Home of the RootLab movies website";
+  const descriptionMessage = "Home of the cocktails website";
 
   return (
     <ApplicationWrapper title={titleMessage} description={descriptionMessage}>
-      <div className="grow flex flex-col justify-center items-center bg-[url('/images/home-bg.jpg')]">
-        <div className="grow bg-black w-full justify-center items-center flex opacity-70">
-          <h1 className="text-7xl font-bold text-white">
-            Bienvenidos a Rootlab Movies
-          </h1>
-        </div>
-      </div>
+      <Carousel
+        autoplay
+        effect="fade"
+        lazyLoad="ondemand"
+        pauseOnHover={false}
+        centerMode
+        centerPadding="350px"
+      >
+        <Image
+          src={slice1}
+          alt="Slice 1"
+          width={2000}
+          height={1333}
+          placeholder="blur"
+          layout="responsive"
+        />
+        <Image
+          src={slice2}
+          alt="Slice 2"
+          width={2000}
+          height={1333}
+          placeholder="blur"
+          layout="responsive"
+        />
+        <Image
+          src={slice3}
+          alt="Slice 3"
+          width={2000}
+          height={1333}
+          placeholder="blur"
+          layout="responsive"
+        />
+        <Image
+          src={slice4}
+          alt="Slice 4"
+          width={2000}
+          height={1333}
+          placeholder="blur"
+          layout="responsive"
+        />
+        <Image
+          src={slice5}
+          alt="Slice 5"
+          width={2000}
+          height={1333}
+          placeholder="blur"
+          layout="responsive"
+        />
+      </Carousel>
     </ApplicationWrapper>
   );
 };
