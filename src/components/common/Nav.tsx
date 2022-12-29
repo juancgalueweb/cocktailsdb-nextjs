b6755714319a448dca28853781f0963622ebca30
juancgalueweb <juancgalue@icloud.com>
2022-12-28 22:00:39 -0300

    Adding some spinners and loaders while fetching data

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,11 +1,11 @@
-import { AutoComplete } from 'antd';
+import { AutoComplete, message } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import ingredients from '../../json/list-all-ingredients.json';
 
 export const Nav: FC = () => {
-  // const [val, setVal]
+  const [messageApi, contextHolder] = message.useMessage();
   const router = useRouter();
   const options = ingredients.map((ele) => {
     return { label: ele.strIngredient1, value: ele.strIngredient1 };
@@ -20,29 +20,39 @@ export const Nav: FC = () => {
 
   const onSelect = (value: string) => {
     router.push(`/search-results?ingredient=${value}`);
+    messageApi.open({
+      type: 'loading',
+      content: 'Searching for the cocktails...',
+      duration: 0,
+    });
+    // Dismiss manually and asynchronously
+    setTimeout(messageApi.destroy, 1000);
   };
 
   return (
-    <nav className='w-full flex p-2 pl-4 items-center text-xl justify-between'>
-      <div>
-        <Link href='/' className='mr-4'>
-          Home
-        </Link>
-        <Link href='/popular-cocktails' className='ml-4'>
-          Pupular Cocktails
-        </Link>
-      </div>
-      <div>
-        <AutoComplete
-          dropdownMatchSelectWidth={252}
-          style={{ width: 300 }}
-          placeholder='Search cocktail by ingredient'
-          options={optionsInOrder}
-          filterOption={true}
-          onSelect={onSelect}
-          notFoundContent='Ingredient not found'
-        ></AutoComplete>
-      </div>
-    </nav>
+    <>
+      {contextHolder}
+      <nav className='w-full flex p-2 pl-4 items-center text-xl justify-between'>
+        <div>
+          <Link href='/' className='mr-4'>
+            Home
+          </Link>
+          <Link href='/popular-cocktails' className='ml-4'>
+            Pupular Cocktails
+          </Link>
+        </div>
+        <div>
+          <AutoComplete
+            dropdownMatchSelectWidth={252}
+            style={{ width: 300 }}
+            placeholder='Search cocktail by ingredient'
+            options={optionsInOrder}
+            filterOption={true}
+            onSelect={onSelect}
+            notFoundContent='Ingredient not found'
+          ></AutoComplete>
+        </div>
+      </nav>
+    </>
   );
 };
--- src/pages/search-results/index.tsx
+++ src/pages/search-results/index.tsx
@@ -1,8 +1,10 @@
 // import type { PaginationProps } from 'antd';
 // import { Pagination } from 'antd';
+import { Spin } from 'antd';
 import { GetServerSideProps, NextPage } from 'next';
+import Router from 'next/router';
 import { getPlaiceholder } from 'plaiceholder';
-// import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { CocktailsByIngCard } from '../../components/CocktailsByIng/CocktailByIngCard';
 import { ApplicationWrapper } from '../../components/layout/ApplicationWrapper';
@@ -21,17 +23,35 @@ const CocktailsByIngSearchResult: NextPage<TProps> = ({
   const titleMessage = `Cocktails with ${ingredient}`;
   const descriptionMessage = 'Results of the cocktails by searched ingredient';
 
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => setLoading(true);
+    const end = () => setLoading(false);
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <ApplicationWrapper title={titleMessage} description={descriptionMessage}>
       <div className='flex flex-col justify-center items-center p-6 bg-slate-200'>
         <h1 className='text-4xl pb-6 font-semibold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600'>
           Cocktails made of {ingredient}
         </h1>
-        <ul className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-3'>
-          {drinks.map((drink) => (
-            <CocktailsByIngCard key={uuidv4()} drink={drink} />
-          ))}
-        </ul>
+        {loading ? (
+          <Spin size='large' />
+        ) : (
+          <ul className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-3'>
+            {drinks.map((drink) => (
+              <CocktailsByIngCard key={uuidv4()} drink={drink} />
+            ))}
+          </ul>
+        )}
       </div>
     </ApplicationWrapper>
   );
