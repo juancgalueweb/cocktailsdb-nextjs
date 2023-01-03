db6dfdf333e2bceb77f4445acde650bafb0b3172
juancgalueweb <juancgalue@icloud.com>
2023-01-03 16:41:00 -0300

    Adding dynamic pagination - still some bugs to fix

--- src/pages/search-results/index.tsx
+++ src/pages/search-results/index.tsx
@@ -1,5 +1,8 @@
+import type { PaginationProps } from 'antd';
+import { Pagination } from 'antd';
 import { GetServerSideProps, NextPage } from 'next';
 import { getPlaiceholder } from 'plaiceholder';
+import { useCallback, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { CocktailsByIngCard } from '../../components/CocktailsByIng/CocktailByIngCard';
 import { ApplicationWrapper } from '../../components/layout/ApplicationWrapper';
@@ -30,6 +33,37 @@ const CocktailsByIngSearchResult: NextPage<TProps> = ({
 }) => {
   const titleMessage = `Cocktails with ${ingredient}`;
   const descriptionMessage = 'Results of the cocktails by searched ingredient';
+  const [current, setCurrent] = useState(1);
+
+  const chunks = useCallback((allDrinks: ICocktailsByIng[]) => {
+    let newArray: ICocktailsByIng[] = [];
+    const finalArray: ICocktailsByIng[][] = [];
+    if (allDrinks.length <= 10) {
+      finalArray.push(allDrinks);
+    } else {
+      allDrinks.forEach((drink, idx) => {
+        newArray.push(drink);
+        if ((idx + 1) % 10 === 0) {
+          finalArray.push(newArray);
+          newArray = [];
+        } else if (idx === allDrinks.length - 1 && newArray.length !== 0) {
+          finalArray.push(newArray);
+        }
+      });
+    }
+    return finalArray;
+  }, []);
+
+  const [dataToShow, setDataToShow] = useState(chunks(drinks)[0]);
+  const onChange: PaginationProps['onChange'] = (page) => {
+    setDataToShow(chunks(drinks)[page - 1]);
+    setCurrent(page);
+  };
+
+  useEffect(() => {
+    setDataToShow(chunks(drinks)[0]);
+    setCurrent(1);
+  }, [drinks, chunks]);
 
   return (
     <ApplicationWrapper title={titleMessage} description={descriptionMessage}>
@@ -38,10 +72,18 @@ const CocktailsByIngSearchResult: NextPage<TProps> = ({
           Cocktails made of {ingredient}
         </h1>
         <ul className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-3'>
-          {drinks.map((drink) => (
+          {dataToShow.map((drink) => (
             <CocktailsByIngCard key={uuidv4()} drink={drink} />
           ))}
         </ul>
+        <Pagination
+          hideOnSinglePage
+          simple
+          pageSize={10}
+          current={current}
+          onChange={onChange}
+          total={drinks.length}
+        />
       </div>
     </ApplicationWrapper>
   );
