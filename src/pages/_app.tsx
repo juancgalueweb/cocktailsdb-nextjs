33a7428e7716c6de8361ddc1617404c9739feeb0
juancgalueweb <juancgalue@icloud.com>
2023-02-07 11:41:23 -0300

    Disable spinner in Nprogress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,25 @@
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import { useEffect } from 'react';
-import '../styles/global.css';
-config.autoAddCss = false;
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { useEffect } from 'react'
+import '../styles/global.css'
+config.autoAddCss = false
 
-import type { AppProps } from 'next/app';
+NProgress.configure({ showSpinner: false })
+
+import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     Router.events.on('routeChangeStart', () => {
-      NProgress.start();
-    });
+      NProgress.start()
+    })
     Router.events.on('routeChangeComplete', () => {
-      NProgress.done();
-    });
-  }, []);
-  return <Component {...pageProps} />;
+      NProgress.done()
+    })
+  }, [])
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
