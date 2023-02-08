19725b6840f9ed3f015039fa929b5f702814e327
juancgalueweb <juancgalue@icloud.com>
2023-02-08 01:52:15 -0300

    Remove dark colors from the Footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,12 +5,11 @@ import { FC } from 'react'
 
 export const Footer: FC = () => {
   return (
-    <footer className='p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
-      <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+    <footer className='p-4 bg-white shadow md:flex md:items-center md:justify-between'>
+      <span className='text-sm text-gray-500 sm:text-center'>
         Project created by Juan C. Galu&eacute; R.
       </span>
-
-      <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+      <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0'>
         <li>
           <a
             href='mailto: juancgalue@gmail.com?subject=Email%20from%20NextJS%20Vercel%20App'
