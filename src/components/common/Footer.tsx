2b981be2b9585a21ea2340a6ef358bc8956383f8
juancgalueweb <juancgalue@icloud.com>
2023-02-08 01:15:59 -0300

    Remove rounded border from Footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react'
 
 export const Footer: FC = () => {
   return (
-    <footer className='p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
+    <footer className='p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
       <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
         Project created by Juan C. Galu&eacute; R.
       </span>
